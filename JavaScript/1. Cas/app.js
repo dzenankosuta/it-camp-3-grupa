96b4bba0d9d1ac0a0543ec1e684a75f804470f47
dzenankosuta <dzenan_kosuta@hotmail.com>
2023-03-25 11:34:42 +0100

    Tipovi podataka - drugi deo

--- JavaScript/1. Cas/app.js
+++ JavaScript/1. Cas/app.js
@@ -84,3 +84,18 @@ obj = {
 
 console.log(obj);
 console.log(typeof obj);
+
+// 3. Set - Struktura podataka za skladistenje vise vrednosti koja ne dozvoljava duplikate. Slicna je nizu (Array), a sintaksa za pravljenje seta je:
+
+set = new Set(["Asija", "Melida", "Dino", 19, 31, 25, "Dino"]); // Svako novo pojavljivanje elementa u setu se nece primeniti.
+console.log(set);
+
+// 4. Map - Struktura podataka za skladistenje vise vrednosti i slicna je objektu. Sintaksa za pravljenje mape je:
+
+map = new Map([
+  ["banana", 160],
+  ["jagoda", 240],
+  ["kruska", 140],
+  ["ananas", 289],
+]);
+console.log(map);
