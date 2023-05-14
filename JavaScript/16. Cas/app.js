913fc7d36a7adb38aec22bae5f6f4fdbc1975684
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2023-05-14 10:34:33 +0200

    domaci

--- JavaScript/16. Cas/app.js
+++ JavaScript/16. Cas/app.js
@@ -94,15 +94,18 @@ function myFunc(recenica) {
 console.log(myFunc("Recenica za modifikaciju."));
 console.log(myFunc("Nije do"));
 
-// Domaci zadatak.
+// Domaci zadaci:
 // Napraviti funkciju koja trazi od korisnika da unese neku recenicu, zatim funkcija vraca, datu recenicu u vise oblika (spojenu u jedan string).
-// 1. recenicu ispisanu velikim slovima,
-// 2. recenicu ispisanu malim slovima,
-// 3. pola recenice ispisano velikim slovima, pola recenice ispisano malim slovima
-// 4. Ako se u recenici nadje rec 'skola' neka bude zamenjena recju fakultet.
+// 1. recenicu ispisanu velikim slovima;
+// 2. recenicu ispisanu malim slovima;
+// 3. pola recenice ispisano velikim slovima, pola recenice ispisano malim slovima;
+// 4. Ako se u recenici nadje rec 'skola' neka bude zamenjena recju 'fakultet'.
 
 // Zadatak: Zamena reči
-// Napiši funkciju koja prima dva argumenta: originalni string i reč koju treba zameniti. Funkcija treba da zameni sve instance te reči u originalnom stringu sa znakom "#" i vrati rezultujući string. Na primer, za ulazne vrednosti "Danas je lep dan." i "lep", funkcija treba da vrati "Danas je # dan.".
+// Napiši funkciju koja prima dva argumenta: originalni string i reč koju treba zameniti.
+// Funkcija treba da zameni sve instance te reči u originalnom stringu sa znakom "#" i vrati rezultujući string.
+// Na primer, za ulazne vrednosti "Danas je lep dan." i "lep", funkcija treba da vrati "Danas je # dan.".
 
 // Zadatak: Brojanje karaktera
-// Napiši funkciju koja prima jedan string kao argument i vraća broj karaktera u tom stringu. Ignoriši prazne prostore. Na primer, za ulazni string "Hello, World!", funkcija treba da vrati broj 12.
+// Napiši funkciju koja prima jedan string kao argument i vraća broj karaktera u tom stringu. Ignoriši prazne prostore.
+// Na primer, za ulazni string "Hello, World!", funkcija treba da vrati broj 12.
